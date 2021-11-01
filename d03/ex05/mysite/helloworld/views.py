87c80ce12994bb184158ebe2b2a7f4ca0998887c
aquinoa-nba <aquinoa@student.21-school.ru>
2021-11-01 21:35:09 +0300

    d03/ex05 added style for helloworld app

--- d03/ex05/mysite/helloworld/views.py
+++ d03/ex05/mysite/helloworld/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello World !')
+    return HttpResponse(""
+                        "<title>Greetings</title>"
+                        "<h1 style='text-align: center; margin: 2%'>Hello World !</h1>"
+                        "<hr>"
+                        "")
