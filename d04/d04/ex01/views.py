7db9e8a1eea81d06a1449ab6648fc93f4454bcc6
aquinoa-nba <aquinoa@student.21-school.ru>
2021-11-03 22:22:29 +0300

    added dict context to views and variable {{ title }} to templates

--- d04/d04/ex01/views.py
+++ d04/d04/ex01/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 
 
 def django_page(request):
-    return render(request, 'ex01/django_page.html')
+    context = {
+        'title': 'Ex01: Django, framework web.',
+    }
+    return render(request, 'ex01/django_page.html', context=context)
 
 
 def display_page(request):
-    return render(request, 'ex01/display_page.html')
+    context = {
+        'title': 'Ex01: Display process of a static page.',
+    }
+    return render(request, 'ex01/display_page.html', context=context)
 
 
 def templates_page(request):
-    return render(request, 'ex01/templates_page.html')
+    context = {
+        'title': 'Ex01: Template engine.',
+    }
+    return render(request, 'ex01/templates_page.html', context=context)
